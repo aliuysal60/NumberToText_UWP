86c82c3cbea41d13199d72b0506e9043e483588b
Ali UYSAL <a.uysal60@gmail.com>
2019-01-24 13:40:18 +0300

    Add files via upload

---
+++ NumberToTextUWP/MainPage.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace NumberToTextUWP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Number.Text = "8921954,10";
+            string sTutar = Number.Text.Replace('.', ',');      
+            string lira = sTutar.Substring(0, sTutar.IndexOf(',')); //tam 
+            string kurus = sTutar.Substring(sTutar.IndexOf(',') + 1, 2);
+            string yazi = "";
+
+            string[] birler = { "", "BİR", "İKİ", "Üç", "DÖRT", "BEŞ", "ALTI", "YEDİ", "SEKİZ", "DOKUZ" };
+            string[] onlar = { "", "ON", "YİRMİ", "OTUZ", "KIRK", "ELLİ", "ALTMIŞ", "YETMİŞ", "SEKSEN", "DOKSAN" };
+            string[] binler = { "KATRİLYON", "TRİLYON", "MİLYAR", "MİLYON", "BİN", "" }; 
+
+            int grupSayisi = 6; 
+
+            lira = lira.PadLeft(grupSayisi * 3, '0');           
+
+            string grupDegeri;
+
+            for (int i = 0; i < grupSayisi * 3; i += 3) 
+            {
+                grupDegeri = "";
+
+                if (lira.Substring(i, 1) != "0")
+                    grupDegeri += birler[Convert.ToInt32(lira.Substring(i, 1))] + "YÜZ";              
+
+                if (grupDegeri == "BİRYÜZ") 
+                    grupDegeri = "YÜZ";
+
+                grupDegeri += onlar[Convert.ToInt32(lira.Substring(i + 1, 1))]; 
+
+                grupDegeri += birler[Convert.ToInt32(lira.Substring(i + 2, 1))];       
+
+                if (grupDegeri != "") //binler
+                    grupDegeri += binler[i / 3];
+
+                if (grupDegeri == "BİRBİN")
+                    grupDegeri = "BİN";
+
+                yazi += grupDegeri;
+            }
+
+            if (yazi != "")
+                yazi += " TL ";
+
+            int yaziUzunlugu = yazi.Length;
+
+            if (kurus.Substring(0, 1) != "0") //kuruş onlar
+                yazi += onlar[Convert.ToInt32(kurus.Substring(0, 1))];
+
+            if (kurus.Substring(1, 1) != "0") //kuruş birler
+                yazi += birler[Convert.ToInt32(kurus.Substring(1, 1))];
+                Read.Text = yazi += birler[Convert.ToInt32(kurus.Substring(1, 1))];
+
+            if (yazi.Length > yaziUzunlugu)
+                Read.Text = yazi += " Kr.";
+            else
+                yazi += "SIFIR Kr.";
+
+        }
+        
+    }
+}
